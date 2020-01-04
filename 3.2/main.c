ea21396648f90a594cde99dc02871469b9abf0ae
EnoiuDiana <edianagte@yahoo.com>
2020-01-04 10:21:43 +0200

    0

--- 3.2/main.c
+++ 3.2/main.c
@@ -1,19 +1,6 @@
 #include <stdio.h>
 
-int hermite(int n,int x){
-    if(n==0) return 1;
-    else {
-        if(n==1) return 2*x;
-        else return 2*x*hermite(n-1,x)-2*(n-1)*hermite(n-2,x);
-    }
-}
 int main() {
-    int n, Hx, x;
-    printf("Write the values for n:\n");
-    scanf("%d",&n);
-    printf("Write the values for x:\n");
-    scanf("%d",&x);
-    Hx = hermite(n,x);
-    printf("%d",Hx);
+    printf("Hello, World!\n");
     return 0;
 }
